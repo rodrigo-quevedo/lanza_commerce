777e711310306348f7acf7c0a9e84c60edaad646
rodrigo <martinqueve60@gmail.com>
2025-03-25 23:58:01 -0300

    fix(HeaderNavbar): display error of useFetchCategories

--- frontend/src/hooks/useFetchCategories.ts
+++ frontend/src/hooks/useFetchCategories.ts
@@ -1,41 +1,31 @@
 import { useEffect, useState } from "react";
 
-
 export function useFetchCategories() {
     const [categories, setCategories] = useState<FakeStoreCathegory[]>([]);
     const [fetchState, setFetchState] = useState<FetchState>({
         loading: true, //when component mounts, it is loading untill fetch is done
-        error: null});
+        error: null
+    });
     
-  
     useEffect(() => {
-        async function fetchCategories() {
-            try {
-                const response = await fetch("https://api.escuelajs.co/api/v1/categories");
-                
-                if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-                
-                const data: FakeStoreCathegory[] = await response.json();
-                setCategories(data);
+        fetch("https://api.escuelajs.co/api/v1/categories")
+        .then(res=>res.json())
+        
+        .then(data=>{
+            setCategories(data);
+            setFetchState({
+                error: null,
+                loading: false
+            }) })
+        
+        .catch(err=> setFetchState({
+            error:  err.message,
+            loading: false}) )
 
-            } 
-            catch (err) {
-                setFetchState({
-                    ...fetchState,
-                    error:  err instanceof Error ? err.message : "Failed to fetch categories",
-                });
-                
-            } finally {
-                setFetchState({
-                    ...fetchState,
-                    loading: false
-                });
-            }
-      };
-  
-     
-      console.log( fetchCategories())
     }, []); //run once 
   
     return { categories, fetchState };
-  }
+}
+
+
+
--- frontend/src/layouts/MainLayout/HeaderSubLayouts/HeaderNavbar.tsx
+++ frontend/src/layouts/MainLayout/HeaderSubLayouts/HeaderNavbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+
 import { useFetchCategories } from "../../../hooks/useFetchCategories";
 
 export default function HeaderNavbar(){
@@ -7,16 +7,17 @@ export default function HeaderNavbar(){
 
     if (fetchState.loading) {
       return (
-        <nav className="flex justify-center items-center p-4 bg-black text-white">
+        <nav className="flex justify-center items-center p-1 bg-black text-white">
           <span className="animate-pulse">Loading categories...</span>
         </nav>
       );
     }
   
     if (fetchState.error !== null) {
+        console.log("!= null")
       return (
-        <nav className="flex justify-center items-center p-4 bg-black text-red-500">
-          <span>Error: {fetchState.error}</span>
+        <nav className="flex justify-center items-center bg-black text-red-500">
+          <span>Error: {fetchState.error} sldfjlaskfdj</span>
         </nav>
       );
     }
@@ -24,9 +25,9 @@ export default function HeaderNavbar(){
     return (
         <nav 
             className="
-                flex justify-around 
-                gap-6 
-                p-0 
+                flex justify-around items-center
+                overflow-hidden
+                gap-6  
                 w-full
                 font-medium
                 text-sm
@@ -34,24 +35,16 @@ export default function HeaderNavbar(){
         ">
             {categories.map((category) => {
                 return(
-                    <a key={category.id} href="#category1" className=" hover:text-black ">
+                    <a 
+                        key={category.id} 
+                        href="#category1" 
+                        className="hover:scale-110 hover:font-bold hover:text-red-700 
+                                    transition-all hover:transition-all">
                         {category.name}
                     </a>
                 )
             })}
             
-            {/* <a href="#category2" className=" hover:text-black">
-                Category 2
-            </a>
-            <a href="#category3" className=" hover:text-black">
-                Category 3
-            </a>
-            <a
-                href="#special-offers"
-                className="text-red-600 hover:underline"
-            >
-                Special Offers
-            </a> */}
         </nav>
     )
 
